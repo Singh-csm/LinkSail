342cc37ac5ef517bb25c7fb0cea3bff81a003c0a
Singh-csm <csm8107@gmail.com>
2023-06-18 22:10:08 +0530

    cors: added

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 app.use(express.json());
+app.use(cors())
 
 const route = require('./src/routes/routes');
 app.use('/', route);
