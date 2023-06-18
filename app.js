fda37b2b110795cb7bdf92c2d269d4ca9dbd020d
mnu4513 <mnu4513@gmail.com>
2023-06-18 21:49:18 +0530

    .

--- app.js
+++ app.js
@@ -13,4 +13,5 @@ mongoose.connect(mongooseConnectionString).then((result) => {
     console.log(err);
 });
 
-app.listen(3000, () => console.log('listening on port 3000'));
+const port = 3000;
+app.listen(port, () => console.log('listening on port 3000'));
